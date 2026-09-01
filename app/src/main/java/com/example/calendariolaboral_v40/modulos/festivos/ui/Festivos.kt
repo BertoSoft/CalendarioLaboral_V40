21108eed59f355fbea08999da1ab45af6c7c8ad9
alberto <berto.forjan@gmail.com>
2026-09-01 21:36:08 +0200

    010920262136-desptok

--- app/src/main/java/com/example/calendariolaboral_v40/modulos/festivos/ui/Festivos.kt
+++ app/src/main/java/com/example/calendariolaboral_v40/modulos/festivos/ui/Festivos.kt
@@ -1,12 +1,15 @@
 package com.example.calendariolaboral_v40.modulos.festivos.ui
 
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.calendariolaboral_v40.R
 import com.example.calendariolaboral_v40.databinding.ActivityFestivosBinding
+import java.time.LocalDate
 
 class Festivos : AppCompatActivity() {
 
@@ -17,5 +20,33 @@ class Festivos : AppCompatActivity() {
         binding = ActivityFestivosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initUi()
+
+    }
+
+    private fun initUi() {
+        initSp()
+    }
+
+    private fun initSp() {
+        initspAnos()
+        initspFestivos()
+    }
+
+    private fun initspAnos() {
+        val ano = LocalDate.now().year
+        val listaAnos = (ano + 1).downTo(2022).map{ it.toString() }
+        val arrayAdapter = ArrayAdapter(
+            this,
+            R.layout.item_sp_anos,
+            R.id.tvSp,
+            listaAnos
+        )
+        arrayAdapter.setDropDownViewResource(R.layout.item_sp_anos)
+        binding.spAnos.adapter = arrayAdapter
+        binding.spAnos.setSelection(1)
+    }
+
+    private fun initspFestivos() {
     }
 }
