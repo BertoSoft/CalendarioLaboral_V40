99834ea32217905830e077d1e460c1262eeded32
alberto <berto.forjan@gmail.com>
2026-09-14 13:53:51 +0200

    final6

--- app/src/main/java/com/example/calendariolaboral_v40/modulos/home/ui/HomeActivity.kt
+++ app/src/main/java/com/example/calendariolaboral_v40/modulos/home/ui/HomeActivity.kt
@@ -74,7 +74,7 @@ class HomeActivity : AppCompatActivity() {
             cardVacaciones.setTextosyEmojis(R.color.home_vacaciones,"\uD83C\uDF34", "Mis \nVacaciones")
             cardBackup.setTextosyEmojis(R.color.home_backup,"\uD83D\uDCBE", "Copia de \nSeguridad")
             cardExcesoJornadas.setTextosyEmojis(R.color.home_exceso,"⏱\uFE0F", "Exceso de \nJornadas")
-            cardCalendarioLaboral.setTextosyEmojis(R.color.accent_calendario,"\uD83D\uDCC5", "Calendario Laboral")
+            cardCalendarioLaboral.setTextosyEmojis(R.color.home_calendario,"\uD83D\uDCC5", "Calendario Laboral")
         }
     }
 }
